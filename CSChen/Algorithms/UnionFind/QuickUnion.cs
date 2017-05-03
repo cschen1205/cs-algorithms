ef66f9a06aeefe448e4809736261e111f69c7b59
Caishun Chen <cschen1205@gmail.com>
2017-05-03 09:37:34 +0800

    Update the dotnet project from mac

--- CSChen/Algorithms/UnionFind/QuickUnion.cs
+++ CSChen/Algorithms/UnionFind/QuickUnion.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace SimuKit.Algorithms.UnionFind
+namespace CSChen.Algorithms.UnionFind
 {
     public class QuickUnion : IUnionFind
     {
