d2427d808fe2de3851ddef9cb8e200550ac0f9de
Caishun Chen <cschen1205@gmail.com>
2017-05-10 12:24:36 +0800

    Implement selection sort

---
+++ Algorithms/Sorting/SelectionSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Algorithms.Utils;
+
+namespace Algorithms.Sorting
+{
+    public class SelectionSort
+    {
+        public static void Sort<T>(T[] a, int lo, int hi, Comparison<T> comparator)
+        {
+            for (var i = lo; i < hi; ++i)
+            {
+                var max = a[i];
+                var J = i;
+                for (var j = i + 1; j <= hi; ++j)
+                {
+                    if (SortUtil.IsLessThan(a[j], a[J], comparator))
+                    {
+                        J = j;
+                    }
+                }
+                SortUtil.Exchange(a, i, J);
+            }
+        }
+    }
+}
