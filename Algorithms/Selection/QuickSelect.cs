207736772bf4b177abdba22a945a27715d6d0614
Caishun Chen <cschen1205@gmail.com>
2017-05-10 12:04:03 +0800

    Implement quick select

---
+++ Algorithms/Selection/QuickSelect.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Algorithms.Utils;
+
+namespace SimuKit.Algorithms.Selection
+{
+    public class QuickSelect
+    {
+        public static T Select<T>(T[] a, int k, IComparer<T> compareTo = null)
+        {
+            int lo = 0;
+            int hi = a.Length - 1;
+            while (lo < hi)
+            {
+                int j = Partition(a, lo, hi, compareTo);
+                if (j < k) lo = j + 1;
+                else if (j > k) hi = j - 1;
+                else return a[k];
+            }
+
+            return a[k];
+        }
+
+        public static int Partition<T>(T[] a, int lo, int hi, IComparer<T> compareTo)
+        {
+            int i = lo;
+            int j = hi + 1;
+            while (true)
+            {
+                while (SortUtil.IsLessThan(a[++i], a[lo], compareTo))
+                {
+                    if (i == hi) break;
+                }
+                while(SortUtil.IsLessThan(a[lo], a[--j], compareTo))
+                {
+                    if(j==lo) break;
+                }
+
+                if(i >= j) break;
+
+                SortUtil.Exchange(a, i, j);
+            }
+
+            SortUtil.Exchange(a, lo, j);
+            return j;
+
+        }
+    }
+}
---
+++ Algorithms/Utils/SortUtil.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Algorithms.Utils
+{
+    public class SortUtil
+    {
+        public static bool IsLessThan<T>(T p0, T p1, IComparer<T> compareTo)
+        {
+            return compareTo.Compare(p0, p1) < 0;
+        }
+
+        public static bool IsGreaterThan<T>(T p0, T p1, IComparer<T> compareTo)
+        {
+            return compareTo.Compare(p0, p1) < 0;
+        }
+
+        public static void Exchange<T>(T[] a, int i, int j)
+        {
+            var temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+}
