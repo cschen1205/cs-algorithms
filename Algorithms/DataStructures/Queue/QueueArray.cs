6e9b62eff256392e6807c677a89d90afe2b0f6a6
Caishun Chen <cschen1205@gmail.com>
2017-05-12 13:19:37 +0800

    Array implementation of queue

---
+++ Algorithms/DataStructures/Queue/QueueArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Algorithms.DataStructures.Queue
+{
+    public class QueueArray<T> : IQueue<T>
+    {
+        private T[] s = new T[20];
+        private int head = 0;
+        private int tail = 0;
+
+        public T Dequeue()
+        {
+            if (IsEmpty)
+            {
+                return default(T);
+            }
+            var item = s[head++];
+            if (Count < s.Length / 4) Resize(s.Length / 4);
+            return item;
+        }
+
+        private void Resize(int len)
+        {
+            var temp = new T[len];
+            for (var i = head; i < tail; ++i)
+            {
+                temp[i - head] = s[i];
+            }
+            tail = tail - head;
+            head = 0;
+            s = temp;
+        }
+
+        public void Enqueue(T item)
+        {
+            s[tail++] = item;
+            if(tail == s.Length) Resize(s.Length * 2);
+        }
+
+        public int Count => tail - head;
+        public bool IsEmpty => tail - head == 0;
+    }
+}
---
+++ AlgorithmsUnitTest/DataStrutures/Queue/QueueArrayUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using Algorithms.DataStructures.Queue;
+using Xunit;
+
+namespace AlgorithmsUnitTest.DataStrutures.Queue
+{
+    public class QueueArrayUnitTest
+    {
+        [Fact]
+        public void TestQueue()
+        {
+            var queue = new QueueArray<int>();
+            queue.Enqueue(10);
+            Assert.Equal(1, queue.Count);
+            queue.Enqueue(20);
+            Assert.Equal(2, queue.Count);
+            Assert.False(queue.IsEmpty);
+            Assert.Equal(10, queue.Dequeue());
+            Assert.Equal(20, queue.Dequeue());
+            Assert.True(queue.IsEmpty);
+        }
+    }
+}
