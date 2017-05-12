b32f6177e17d9abc6a4052d22cd7c9601a8e0401
Caishun Chen <cschen1205@gmail.com>
2017-05-12 12:54:49 +0800

    Array implementation of Stack

---
+++ Algorithms/DataStructures/Stack/StackArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DataStructures.Stack
+{
+    public class StackArray<T> : IStack<T>
+    {
+        private T[] s = new T[10];
+        private int N = 0;
+
+        public T Pop()
+        {
+            if (N == 0) return default(T);
+            T item = s[--N];
+            if (N == s.Length / 4) Resize(s.Length / 2);
+            return item;
+        }
+
+        private void Resize(int len)
+        {
+            T[] temp = new T[len];
+            for (var i = 0; i < Math.Min(len, s.Length); ++i)
+            {
+                temp[i] = s[i];
+            }
+            s = temp;
+
+        }
+
+        public void Push(T item)
+        {
+            s[N++] = item;
+            if(N == s.Length) Resize(s.Length * 2);
+        }
+
+        public int Count => N;
+        public bool IsEmpty => N == 0;
+    }
+}
---
+++ AlgorithmsUnitTest/DataStrutures/Stack/StackArrayUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using DataStructures.Stack;
+
+namespace AlgorithmsUnitTest.DataStrutures.Stack
+{
+    public class StackArrayUnitTest
+    {
+        [Fact]
+        public void TestStack()
+        {
+            var stack = new StackArray<int>();
+            stack.Push(10);
+            stack.Push(20);
+            Assert.Equal(2, stack.Count);
+            Assert.False(stack.IsEmpty);
+            Assert.Equal(20, stack.Pop());
+            Assert.Equal(10, stack.Pop());
+            Assert.True(stack.IsEmpty);
+        }
+    }
+}
