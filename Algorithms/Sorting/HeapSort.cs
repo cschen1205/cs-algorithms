ddf3d631ba04d2dd91f6f92920f67cc81c4a0ead
Caishun Chen <cschen1205@gmail.com>
2017-05-12 11:47:21 +0800

    Implement heap sort

---
+++ Algorithms/Sorting/HeapSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using Algorithms.Utils;
+
+namespace Algorithms.Sorting
+{
+    public class HeapSort
+    {
+        public static void Sort<T>(T[] a) where T : IComparable<T>
+        {
+            Sort(a, (a1, a2) => a1.CompareTo(a2));
+        }
+
+        public static void Sort<T>(T[] a, Comparison<T> comparison)
+        {
+            var N = a.Length;
+            for (var i = N / 2; i >= 1; --i)
+            {
+                Sink(a, i, N, comparison);
+            }
+            while (N > 0)
+            {
+                SortUtil.Exchange(a, Index(1), Index(N));
+                Sink(a, 1, --N, comparison);
+            }
+        }
+
+        private static void Sink<T>(T[] a, int k, int N, Comparison<T> comparison)
+        {
+            while (k * 2 <= N)
+            {
+                var child = 2 * k;
+                if (child < N && SortUtil.IsGreaterThan(a[Index(child + 1)], a[Index(child)], comparison))
+                {
+                    child++;
+                }
+                if (SortUtil.IsGreaterThan(a[Index(child)], a[Index(k)], comparison))
+                {
+                    SortUtil.Exchange(a, Index(child), Index(k));
+                    k = child;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private static int Index(int j)
+        {
+            return j - 1;
+        }
+    }
+}
---
+++ AlgorithmsUnitTest/Sorting/HeapSortUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using Algorithms.Shuffling;
+using Algorithms.Sorting;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AlgorithmsUnitTest.Sorting
+{
+    public class HeapSortUnitTest
+    {
+        private ITestOutputHelper console;
+        public HeapSortUnitTest(ITestOutputHelper console)
+        {
+            this.console = console;
+        }
+
+        [Fact]
+        public void testSort()
+        {
+            var a = new int[100];
+            var b = new int[100];
+            for (var i = 0; i < a.Length; ++i)
+            {
+                a[i] = i;
+                b[i] = i;
+            }
+            KnuthShuffle.Shuffle(a);
+            Assert.NotEqual(b, a);
+            HeapSort.Sort(a);
+            for (var i = 0; i < a.Length; ++i)
+            {
+                console.WriteLine("{0}", a[i]);
+            }
+            for (var i = 0; i < a.Length; ++i)
+            {
+                Assert.Equal(b[i], a[i]);
+            }
+        }
+    }
+}
