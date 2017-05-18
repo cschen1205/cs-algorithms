47e1b8bd0ef310d546752e7ea32c4eec875778d6
Caishun Chen <cschen1205@gmail.com>
2017-05-18 09:31:49 +0800

    Unit test for longest common pattern

--- Algorithms/Strings/Search/LongestCommonPattern.cs
+++ Algorithms/Strings/Search/LongestCommonPattern.cs
@@ -5,29 +5,9 @@ namespace Algorithms.Strings.Search
 {
     public class LongestCommonPattern
     {
-        public String Find(String s1, String s2)
-        {
-            int N = s1.Length;
-            String[] a = new String[N];
-            for (var i = 0; i < N; ++i)
-            {
-                a[i] = s1.Substring(i, N);
-            }
-            QuickSort.Sort(a);
 
-            String lcs = "";
-            for (var i = 0; i < N; ++i)
-            {
-                String s = LongestCommonString(a[i], s2);
-                if (s.Length > lcs.Length)
-                {
-                    lcs = s;
-                }
-            }
-            return lcs;
-        }
 
-        public String LongestCommonString(String s, String pat)
+        public static String LongestCommonString(String s, String pat)
         {
             int J = 0;
             for (var i = 0; i < s.Length; ++i)
