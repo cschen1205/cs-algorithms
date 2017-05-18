3ce44438248e165ca3f90469a69f9d1efefae972
Caishun Chen <cschen1205@gmail.com>
2017-05-18 09:08:24 +0800

    Unit test for bellman fulkerson algorithm

---
+++ AlgorithmsUnitTest/Graphs/ShortestPaths/BellmanFulkersonUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Algorithms.DataStructures.Graphs;
+using Algorithms.Graphs.ShortestPaths;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AlgorithmsUnitTest.Graphs.ShortestPaths
+{
+    public class BellmanFulkersonUnitTest
+    {
+        private ITestOutputHelper console;
+
+        public BellmanFulkersonUnitTest(ITestOutputHelper console)
+        {
+            this.console = console;
+        }
+
+        [Fact]
+        public void Test()
+        {
+            WeightedDiGraph G = GraphGenerator.directedEdgeWeightedGraph();
+            BellmanFulkerson BellmanFulkerson = new BellmanFulkerson(G, 0);
+            for(var v=1; v < G.V(); ++v)
+            {
+                if (!BellmanFulkerson.HasPathTo(v))
+                {
+                    Console.WriteLine("Path not found for {0}", v);
+                    continue;
+                }
+                IEnumerable<Edge> path = BellmanFulkerson.PathTo(v);
+
+                console.WriteLine(ToString(path));
+                Console.WriteLine(ToString(path));
+            }
+        }
+
+        private String ToString(IEnumerable<Edge> path)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool first = true;
+            foreach (var e in path)
+            {
+                if (first)
+                {
+                    first = false;
+                    sb.Append(e.from());
+                }
+                sb.Append("=(").Append(e.Weight).Append(")=>").Append(e.to());
+            }
+            return sb.ToString();
+        }
+    }
+}
