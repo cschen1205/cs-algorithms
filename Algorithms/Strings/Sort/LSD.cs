64d5bc3ebb2be55de136b9ed33ec942afee64e13
Caishun Chen <cschen1205@gmail.com>
2017-05-19 09:04:41 +0800

    Implement the LSD algorithm for string sorting

---
+++ Algorithms/Strings/Sort/LSD.cs
@@ -0,0 +1,40 @@
+﻿namespace Algorithms.Strings.Sort
+{
+    public class LSD
+    {
+        public static void Sort(string[] a)
+        {
+            var N = a.Length;
+            var M = a[0].Length;
+
+            var R = 256;
+
+            var aux = new string[N];
+
+            for (var d = M-1; d >= 0; --d)
+            {
+                var counts = new int[R + 1];
+
+                for (var i = 0; i < N; ++i)
+                {
+                    counts[a[i][d]+1]++;
+                }
+
+                for (var r = 0; r < R; ++r)
+                {
+                    counts[r + 1] += counts[r];
+                }
+
+                for (var i = 0; i < N; ++i)
+                {
+                    aux[counts[a[i][d]]++] = a[i];
+                }
+
+                for (var i = 0; i < N; ++i)
+                {
+                    a[i] = aux[i];
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmsUnitTest/Strings/Sort/LSDUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Algorithms.Strings.Sort;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AlgorithmsUnitTest.Strings.Sort
+{
+    public class LSDUnitTest
+    {
+        private ITestOutputHelper console;
+        public LSDUnitTest(ITestOutputHelper console)
+        {
+            this.console = console;
+        }
+
+        [Fact]
+        public void Test()
+        {
+
+            var words= "bed bug dad yes zoo now for tip ilk dim tag jot sob nob sky hut men egg few jay owl joy rap gig wee was wad fee tap tar dug jam all bad yet".Split(' ');
+            LSD.Sort(words);
+
+            for (var i = 0; i < words.Length; ++i)
+            {
+                console.WriteLine(words[i]);
+                Console.WriteLine(words[i]);
+            }
+
+        }
+    }
+}
