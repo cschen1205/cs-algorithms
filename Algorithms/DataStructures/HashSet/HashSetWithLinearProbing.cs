286f7cf103c060496359a21de665f8f5a05e3de1
Caishun Chen <cschen1205@gmail.com>
2017-05-19 09:49:36 +0800

    HashSet with linear probing

---
+++ Algorithms/DataStructures/HashSet/HashSetWithLinearProbing.cs
@@ -0,0 +1,17 @@
+﻿using Algorithms.DataStructures.HashMap;
+
+namespace Algorithms.DataStructures.HashSet
+{
+    public class HashSetWithLinearProbing<T> : HashMapWithLinearProbing<T, T>
+    {
+        public void Add(T item)
+        {
+            this[item] = item;
+        }
+
+        public bool Contains(T item)
+        {
+            return ContainsKey(item);
+        }
+    }
+}
