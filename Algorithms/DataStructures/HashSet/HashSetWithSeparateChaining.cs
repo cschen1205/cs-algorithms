ddb061648db316008d012c18a0a17c2a9f4e36af
Caishun Chen <cschen1205@gmail.com>
2017-05-19 09:51:05 +0800

    HashSet with separate chaining

---
+++ Algorithms/DataStructures/HashSet/HashSetWithSeparateChaining.cs
@@ -0,0 +1,17 @@
+﻿using Algorithms.DataStructures.HashMap;
+
+namespace Algorithms.DataStructures.HashSet
+{
+    public class HashSetWithSeparateChaining<T> : HashMapWithSeparateChaining<T,T>
+    {
+        public void Add(T item)
+        {
+            this[item] = item;
+        }
+
+        public bool Contains(T item)
+        {
+            return ContainsKey(item);
+        }
+    }
+}
